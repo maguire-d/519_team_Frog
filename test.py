d354f36c3f0f9efcc777c574c45963f23e2fb16b
GabeFranzi <fgabriel@unc.edu>
2024-11-21 14:52:42 -0500

    Added testing file

---
+++ test.py
@@ -0,0 +1,10 @@
+# Tips and tricks for using github for the unititated:
+# Useful git terminal commands
+    # Pushing to master branch on GitHub in the terminal
+        # git add .
+        # git commit -m "Enter a commit message"
+        # git push
+# Pulling from GitHub
+        # git pull
+    # Restore file to previous save state
+        # git restore [filepath to the file you want to restore]
